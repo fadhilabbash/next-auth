b794a454a8a1af9fbdb3139e8171082610a0a8ab
Fadhil A. Hussein <qamercenter@gmail.com>
2025-02-05 16:15:14 +0300

    under testing

--- auth.ts
+++ auth.ts
@@ -31,30 +31,3 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
   },
 });
-
-
-
-// callbacks: {
-//   authorized({ auth, request: { nextUrl } }) {
-//     const isLoggedIn = !!auth?.user;
-//     const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-//     if (isOnDashboard) {
-//       if (isLoggedIn) return true;
-//       return false; // Redirect unauthenticated users to login page
-//     } else if (isLoggedIn) {
-//       return Response.redirect(new URL('/dashboard', nextUrl));
-//     }
-//     return true;
-//   },
-// },
-// callbacks: {
-//   authorized({ auth, request: { nextUrl } }) {
-//     const isLoggedIn = !!auth?.user;
-//     const publicRoutes = ["/login", "/register"]; // Allow these routes for unauthenticated users
-//     const isPublic = publicRoutes.includes(nextUrl.pathname);
-
-//     if (isPublic) return true; // Allow access to public routes
-//     if (!isLoggedIn) return Response.redirect(new URL('/login', nextUrl)); // Redirect to login
-//     return true;
-//   },
-// },
